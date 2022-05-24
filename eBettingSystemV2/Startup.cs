b14c96ef4f39065d0b28695bc542d874419ad813
AzerPilavdzic <azerpilavdzic@gmail.com>
2022-05-24 12:46:35 +0200

    models

--- eBettingSystemV2/Startup.cs
+++ eBettingSystemV2/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,19 +15,22 @@ using System.Threading.Tasks;
 
 namespace eBettingSystemV2
 {
-    public class Startup
+    public class Startup 
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            //???
+            //configuration.GetConnectionString("DefaultConnection");
+
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
