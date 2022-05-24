ee0dc192c644dd7683bc1ba86f7d644c0a89fbe8
MacanovicIsmar <ismarmacanovi@yahoo.com>
2022-05-24 14:07:57 +0200

    Country Service

---
+++ eBettingSystemV2.Services/CountryService.cs
@@ -0,0 +1,67 @@
+﻿//using AutoMapper;
+//using eBettingSystemV2.Model;
+//using RS2_Tourism_Agency.Model.Request;
+//using RS2_Tourism_Agency.Model.SearchObjects;
+using eProdaja.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using TourismAgency.Services.Database;
+
+namespace TourismAgency.Services
+{
+    public class DrzavaService :
+        BaseCRUDService
+        <
+        object,
+        object,
+        object,
+        object,
+        object
+        >
+        //ICountryService
+    {
+        public DrzavaService(/*TourismAgency.Services.Database.Rs2_SeminarskiContext context_, IMapper mapper_*/) : base(/*context_, mapper_*/)
+        {
+
+            //context = context_;
+            //Mapper = mapper_;
+
+
+
+        }
+
+
+        //filter model
+        //public override IQueryable<object> AddFilter(/*IQueryable<Drzava> query, DrzavaSearchObject search = null*/)
+        //{
+        //    //var filterquery = base.AddFilter(query, search);
+
+        //    //if (!string.IsNullOrWhiteSpace(search?.NazivGT))
+        //    //{
+
+        //    //    filterquery = filterquery.Where(X => X.Naziv.StartsWith(search.NazivGT));
+
+
+        //    //}
+        //    //return filterquery;
+
+        //    IQueryable<object> T=null;
+
+        //    return T;
+
+        //}
+
+
+
+
+
+
+    }
+}
+
+//inheriting base crud service 
+//using Interface IDrzava 
+//needs context and mapper intalized into crud
