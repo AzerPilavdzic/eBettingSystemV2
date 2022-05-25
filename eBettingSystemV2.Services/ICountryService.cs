7abea6c1238423f91eb044fc804777b9b80fc417
MacanovicIsmar <ismarmacanovi@yahoo.com>
2022-05-25 09:53:37 +0200

    Pull

---
+++ eBettingSystemV2.Services/ICountryService.cs
@@ -0,0 +1,30 @@
+﻿
+using eBettingSystemV2.Models;
+using eProdaja.Services;
+using RS2_Tourism_Agency.Model.SearchObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourismAgency.Services
+{
+    public interface ICountryService : 
+        ICRUDService<CountryModel, 
+            CountrySearchObject, 
+            object, object>
+    {
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
