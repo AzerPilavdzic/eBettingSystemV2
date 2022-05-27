472f6a40d9d40036ceea1d39a697ee0fce7450c8
AzerPilavdzic <azerpilavdzic@gmail.com>
2022-05-27 11:41:12 +0200

    poruka

--- eBettingSystemV2/Controllers/WeatherForecastController.cs
+++ eBettingSystemV2/Controllers/WeatherForecastController.cs
@@ -9,7 +9,9 @@ using System.Threading.Tasks;
 namespace eBettingSystemV2.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/api/v{version:apiVersion}/weatherforecast")]
+    [ApiVersion("1.0", Deprecated = true)] //za testiranje api verzije
+    [ApiVersion("2.0")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -25,22 +27,41 @@ namespace eBettingSystemV2.Controllers
         }
 
         [HttpGet]
+        [MapToApiVersion("1.0")]
         [NotImplExceptionFilter]
-        public async Task<IEnumerable<WeatherForecast>> Get()
+        public async Task<IEnumerable<WeatherForecast>> GetAPIVersion1()
         {
-            
-
             var rng = new Random();
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(-200, 5500),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
         }
 
-        
+
+        //this action will be implemented in other folder 
+        [HttpGet("{id}")]
+        [MapToApiVersion("2.0")]
+        [NotImplExceptionFilter]
+        public async Task<IEnumerable<WeatherForecast>> GetAPIVersion2()
+        {
+            var rng = new Random();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-200, 5500),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        }
+
         //[HttpGet]
         //public IEnumerable<WeatherForecast> GetContact(int id)
         //{
@@ -53,9 +74,5 @@ namespace eBettingSystemV2.Controllers
         //    })
         //    .ToArray();
         //}
-
-
-
-
     }
 }
