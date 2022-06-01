4f3d7a47e35408b8ff7a686b92edf12ac482c1a6
MarkoSopta <philoyd99@gmail.com>
2022-06-01 12:40:34 +0200

    bog zna sta

---
+++ eBettingSystemV2.Model/Models/SportsModelLess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace eBettingSystemV2.Models
+{
+    public partial class SportModelLess
+    {
+        public int SportId { get; set; }
+
+    }
+}
