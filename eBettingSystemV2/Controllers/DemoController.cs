278c7a2db80fbaa4a418f267bffb0aa182bfe10f
MacanovicIsmar <ismarmacanovi@yahoo.com>
2022-06-09 15:27:26 +0200

    Cache prvi dio

--- eBettingSystemV2/Controllers/DemoController.cs
+++ eBettingSystemV2/Controllers/DemoController.cs
@@ -8,7 +8,9 @@ using eBettingSystemV2.Services.Interface;
 using eProdaja.Controllers;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using RezultatiImporter.Cache;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,17 +21,18 @@ namespace eBettingSystemV2.Controllers
     [ApiController]
     [Route("[controller]")]
     //public class CountryController
-    public class DemoController
+    public class DemoController:Controller
     {
 
         private IDemo IDemoService { get; set; }
+        private IMemoryCache _cache { get; set;}
 
-        public DemoController(IDemo service)
+       
+
+        public DemoController(IDemo service, IMemoryCache memoryCache)
         {
             IDemoService = service;
-          
-        
-        
+            _cache = memoryCache;
         }
 
 
@@ -48,10 +51,55 @@ namespace eBettingSystemV2.Controllers
           
         }
 
-       
+        [HttpGet]
+        public string CacheTryGetValueSet()
+        {
+            DateTime? cacheEntry=null;
+
+            // Look for cache key.
+            if (!_cache.TryGetValue(CacheKeys.Entry, out cacheEntry))
+            {
+                // Key not in cache, so get data.
+                cacheEntry = DateTime.Now;
+
+                // Set cache options.
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    // Keep in cache for this time, reset time if accessed.
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(3));
+
+                // Save data in cache.
+                _cache.Set(CacheKeys.Entry, cacheEntry, cacheEntryOptions);
+            }
+
+            string uslov = cacheEntry == null ? "No cached entry found" : cacheEntry.Value.TimeOfDay.ToString();
 
 
-           
-        
+            string Text = $"Current Time: {cacheEntry.Value.ToString()} \n" +
+                          $"Cache  Time :{uslov} \n";
+                
+
+
+                //$"Current Time: \n"+
+                //   $"Cached Time:" +
+                //   cacheEntry == null ? "No cached entry found" : cacheEntry.Value.TimeOfDay.ToString() + "\n";
+
+
+
+            return Text;
+
+
+
+                      
+
+
+
+        }
+
+
+
+
+
+
+
     }
 }
