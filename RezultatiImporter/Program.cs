574b1074787888329c454f5b89227cccb5a9d38d
MacanovicIsmar <ismarmacanovi@yahoo.com>
2022-06-20 11:30:52 +0200

    Api service

--- RezultatiImporter/Program.cs
+++ RezultatiImporter/Program.cs
@@ -1,4 +1,5 @@
-﻿using HtmlAgilityPack;
+﻿using eBettingSystemV2.Model.Models;
+using HtmlAgilityPack;
 using RezultatiImporter.Services;
 using System;
 using System.Collections.Generic;
@@ -14,188 +15,52 @@ namespace RezultatiImporter
       
         private static Timer aTimer;
 
+        public ApiService Service_ { get; set; } = new ApiService();
 
 
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var listaSportova = RezultatiService.FetchAllSports();
             foreach (var item in listaSportova)
             {
-            RezultatiService.FetchDataBySport(item);
+              RezultatiService.FetchDataBySport(item);
             }
             //FetchAll();
 
-            Console.ReadKey(); 
+
+            //await ApiService.Post<PodaciSaStranice>(new PodaciSaStranice { Competitionname = "Test123", Country = "Test123", Sport = "Test123" });
+            Console.ReadKey();
 
 
+            var list = new List<PodaciSaStranice>();
+            list.Add(new PodaciSaStranice { Competitionname = "Test444", Country = "Test444", Sport = "Test444" });
+
+            var list2 = new List<PodaciSaStranice>();
+            list2.Add(new PodaciSaStranice { Competitionname = "Test555", Country = "Test444", Sport = "Test444" });
+
+            var list3 = new List<PodaciSaStranice>();
+            list3.Add(new PodaciSaStranice { Competitionname = "Test666", Country = "Test444", Sport = "Test444" });
 
             SetTimer();
+            await ApiService.Post<PodaciSaStranice>(list);
+            await ApiService.Post<PodaciSaStranice>(list2);
+            await ApiService.Post<PodaciSaStranice>(list3);
+
+
+
+           
             Console.WriteLine("\nPress the Enter key to exit the application...\n");
             Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
             Console.ReadLine();
             aTimer.Stop();
             aTimer.Dispose();
             Console.WriteLine("Terminating the application...");
+            //}
+            Console.WriteLine("========================================================");
 
-
-            //const string str = "Name: music mix.mp3 Size: 2356KB";
-            //var match = Regex.Match(str, @"(\w*\:)");
-            //Console.WriteLine(match.Groups[1].Value);
-
-            //Console.ReadKey();
-
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument document = web.Load("https://m.rezultati.com/");
-
-
-
-            //var title = document.DocumentNode.SelectNodes("//div/h1").First().InnerText;
-            //var description = document.DocumentNode.SelectNodes("//div/p").First().InnerText;
-            //*[@id="score-data"]
-
-            var categories = document.DocumentNode.SelectNodes("//*[@id='score-data']/h4");
-
-            //*[@id="main"]/h2
-            var _sport = document.DocumentNode.SelectSingleNode("//*[@id='main']/h2").InnerText;
-            //var _sport = document.DocumentNode.SelectNodes("//*[@id='score-data']/h4");
-
-            Console.WriteLine(_sport.ToString());
-
-
-            Console.ReadKey();
-
-            var competitions = categories;
             
-            List<string> listaKategorija = new List<string>();
-            List<string> listaNatjecanja = new List<string>();
-            List<string> doDvotacke = new List<string>();
-
-            //Console.WriteLine(title);
-            Console.WriteLine();
-
-
-            //Console.WriteLine(categories);
-            //do ove linije categories nije lista
-
-
-
-            //categories.ToList().ForEach(i => Console.WriteLine(i.InnerText));
-            categories.ToList().ForEach(i => listaKategorija.Add(i.InnerText));
-            competitions.ToList().ForEach(i => listaNatjecanja.Add(i.InnerText));
-
-            //string _regex = @"\w*\:";
-            //jedna rijec 
-            var _regexSport = Regex.Match(_sport, "^\\S*");
-                
-            Console.Write(_regexSport.ToString());
-            Console.ReadKey();
-            //Console.Write(Regex.Match(_sport, "^(\S*)[\s]+.*").ToString());
-
-
-            for (int i = 0; i < listaKategorija.Count(); i++)
-            {
-                //var _match = Regex.Match(lista[i], @"(\w*\:)");
-                //var _match = Regex.Match(lista[i], @"^.*?(?=:)");
-                //var _CompetitionName = Regex.Match(listaNatjecanja[i], @"",RegexOptions.RightToLeft);
-                var _match = Regex.Match(listaKategorija[i], @"(\w*)(?=:)");
-                var _matchCompetitions = Regex.Match(listaNatjecanja[i], @"(?<=: )(?:(?! -).)*");
-
-                listaKategorija[i] = _match.ToString();
-
-                listaNatjecanja[i] = _matchCompetitions.ToString();
-                //listaNatjecanja[i] = _CompetitionName.ToString();
-                //Console.WriteLine(lista[i]);
-            }
-
-
-            //klasa
-            //List<Competitions> Competitions= new List<Competitions>();
-
-
-
-            HashSet<string> CategoryHashSet = new HashSet<string>();
-            HashSet<string> CompetitionHashSet = new HashSet<string>();
-
-            foreach (var item in listaKategorija)
-            {
-                CategoryHashSet.Add(item);
-                //lista.Distinct().ToList();
-            }
-            foreach (var item in listaNatjecanja)
-            {
-                CompetitionHashSet.Add(item);
-                //lista.Distinct().ToList();
-            }
-
-
-            //for (int i = 0; i < KategorijaHashSet.Count(); i++)
-            //{
-            //    Console.WriteLine()
-            //    for (int j = 0; j < NatjecanjaHashSet.Count(); j++)
-            //    {
-            //    }
-            //}
-            Console.WriteLine("========================================================");
-            //foreach (var category in CategoryHashSet)
-            //{
-            //    Console.WriteLine(category);
-            //}
-
-            foreach (var item in CompetitionHashSet)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("========================================================");
-            //}
-            Console.WriteLine("========================================================");
-            //foreach (var category in CategoryHashSet)
-            //{
-            //    Console.WriteLine(category);
-            //}
-
-
-
-            foreach (var item in CategoryHashSet)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("========================================================");
-
-
-            //Console.ReadKey();
-            //foreach (var item in lista)
-            //{
-            //    //do dvotacke
-            //    var _match = Regex.Match(item, @"(\w*\:)");
-            //    GroupCollection groupCollection = _match.Groups;
-            //    //item = groupCollection[0].ToString();
-            //    //Console.WriteLine(item);
-            //    //item.Split()
-            //}
-
-            //foreach (var item in doDvotacke)
-            //{
-            //Console.WriteLine(item);
-            //    //Console.WriteLine(item.Split(":"));
-            //}
-
-
-            ////foreach (var item in lista)
-            ////{
-            ////    Console.WriteLine(item);
-            ////}
-
-
-            ////press any key to continue
-            //Console.ReadKey();
-
-            //foreach (var item in lista)
-            //{
-            //Console.WriteLine(item);
-            //}
+            
 
 
 
@@ -214,6 +79,12 @@ namespace RezultatiImporter
         {
             Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
                               e.SignalTime);
+
+
+            
+
+           
+
         }
 
 
---
+++ RezultatiImporter/Services/ApiService.cs
@@ -0,0 +1,57 @@
+﻿using eBettingSystemV2.Model.Models;
+using eBettingSystemV2.Models;
+using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezultatiImporter.Services
+{
+    public class ApiService
+    {
+        private static string _resource = "CacheLearn/";
+        public static string _endpoint = "https://localhost:44318/";
+        public static string _Command = "TakeDataFromCache";
+
+
+
+        public static async Task Post<T>(List<T> request)
+        {
+           
+
+
+            try
+            {
+                List<PodaciSaStranice> result = await $"{_endpoint}{_resource}{_Command}".PostJsonAsync(request).ReceiveJson<List<PodaciSaStranice>>();
+                Console.WriteLine(result[0].Competitionname.ToString());
+                //return result;
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                Console.WriteLine("Greška");
+
+
+                //return default(T);
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
