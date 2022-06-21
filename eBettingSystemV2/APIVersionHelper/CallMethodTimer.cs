2d3e8becf1d5d09a77ca66927e252163fbac565f
AzerPilavdzic <azerpilavdzic@gmail.com>
2022-06-21 15:29:02 +0200

    timer for api

---
+++ eBettingSystemV2/APIVersionHelper/CallMethodTimer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eBettingSystemV2.APIVersionHelper
+{
+    public class CallMethodTimer
+    {
+
+        public static void Tajmer(int seconds, Func<object>methodName)
+        {
+            var _startTimeSpan = TimeSpan.Zero;
+            var _period = TimeSpan.FromSeconds(seconds);
+            int i = 0;
+            var timer = new System.Threading.Timer((e) =>
+            {
+                Console.WriteLine("test poziv " + i++);
+                methodName();
+
+            }, null, _startTimeSpan, _period);
+        }
+    }
+}
