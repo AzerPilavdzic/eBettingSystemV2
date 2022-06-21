570469bbf645502c804c03558e953673d745e116
MacanovicIsmar <ismarmacanovi@yahoo.com>
2022-06-21 12:20:06 +0200

    database update

---
+++ eBettingSystemV2.Services/DataBase/Logcompetition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace eBettingSystemV2.Services.DataBase
+{
+    public partial class Logcompetition
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public DateTime Column1 { get; set; }
+        public int? Updated { get; set; }
+    }
+}
