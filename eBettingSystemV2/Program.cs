76f603329480399e64e4ffaf8bcc77553ff7b933
AzerPilavdzic <azerpilavdzic@gmail.com>
2022-06-23 09:38:59 +0200

    using eBettingSystemV2.Services.Interface;

--- eBettingSystemV2/Program.cs
+++ eBettingSystemV2/Program.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using eBettingSystemV2.Services.Interface;
 using HtmlAgilityPack;
 using eBettingSystemV2.APIVersionHelper;
 using System.Threading;
